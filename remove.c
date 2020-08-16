113808d3efdeaa26932a9547e6a776ea2ba34d09
Ashwin <bsashwin9@gmail.com>
2020-08-16 19:20:40 +0530

    Update remove command

--- remove.c
+++ remove.c
@@ -1,30 +1,3 @@
-// #include <stdio.h>
-// #include <stdlib.h>
-
-// void usage()
-// {
-//     printf("Usage: remove <file_name>\n");
-// }
-
-// int main(int argc, char const *argv[])
-// {
-//     if (argc != 2)
-//     {
-//         usage();
-//         return EXIT_FAILURE;
-//     }
-
-//     if (!remove(argv[1]))
-//         printf("File deleted successfully!\n");
-//     else
-//     {
-//         perror("Error");
-//         return EXIT_FAILURE;
-//     }
-
-//     return EXIT_SUCCESS;
-// }
-
 #define _POSIX_C_SOURCE 200809L
 #define _XOPEN_SOURCE 700
 
